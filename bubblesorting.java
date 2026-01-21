e7f57e3a8c2fc15115c656c3b743f2c837b1319f
Muntazir Abbas <abbasmuntazir62@gmail.com>
2026-01-21 15:14:43 +0530

    Add files via upload

---
+++ bubblesorting.java
@@ -0,0 +1,24 @@
+public class bubblesorting {
+    public static void printarray(int arr[]){
+        for (int i =0;i < arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+public static void main(String[]args){
+    int arr[] = {7,8,1,2,3};
+  for (int i = 0;i < arr.length-1;i++) {
+    for (int j = 0;j < arr.length-i-1;j++) {
+        if(arr[j] > arr[j+1]){
+            //swap
+        int tempt = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = tempt;
+    
+}
+  }
+}
+     printarray(arr);
+
+}
+}
+    
